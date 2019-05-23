b9e370da9d3716e722f3f7ca7e0bf9b03cc8ae63
Arturo <grabowski.a93@gmail.com>
2019-05-23 15:31:37 +0200

    Inheritance - Animal and other basics

---
+++ React_testing/es6/classes.js
@@ -0,0 +1,31 @@
+class Animal{
+
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
+    }
+
+    speak(){
+        /*console.log("Hi, I'm " + this.name + " and I'm " + this.color);*/
+        console.log(`Hi I'm ${this.name} and I'm ${this.color}`);
+    }
+}
+
+class Lion extends Animal{
+    constructor(name,color, role, home) {
+        super(name, color)
+        this.role = role;
+        this.home = home;
+    }
+
+    roar(){
+        console.log(`I'm the ${this.role} of ${this.home}`);
+
+    }
+}
+
+let lion = new Lion("Mufasa", "golden", "king" , "Pride rock");
+
+console.log(lion);
+lion.speak();
+lion.roar();
